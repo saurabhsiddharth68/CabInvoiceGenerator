bdddf6fc3e53ad3a9056188b33a5c7489ee4167d
Saurabh Siddharth <saurabhsiddharth68>
2021-11-10 00:51:02 +0530

    Added:Added Calculate Fare

---
+++ src/main/java/com/blz/invoice/Cabinvoice.java
@@ -0,0 +1,12 @@
+package com.blz.invoice;
+
+public class Cabinvoice {
+    private static final double COST_PER_KM = 10;
+    private static final double COST_PER_MIN = 1;
+    private static final double MIN_FARE = 5.0;
+
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * COST_PER_KM + time * COST_PER_MIN;
+        return Math.max(totalFare, MIN_FARE);
+    }
+}
